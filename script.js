06522be9385296a10be8364899ac7a59572c9377
Kiranendra <vivs8899@gmail.com>
2020-11-19 17:48:58 +0530

    Minor changes

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+JSON DUMMY DATA: https://jsonplaceholder.typicode.com/
+
+TUTORIAL: https://levelup.gitconnected.com/all-possible-ways-of-making-an-api-call-in-plain-javascript-c0dee3c11b8b
+
+COVID API: https://covid19api.com/
+FOREIGN EXCHANGE RATES: https://fixer.io/
+GLOBAL STOCK MARKET: https://marketstack.com/
+
+*/
